5b89e7f542b463305925feb59336c5ae1258dffb
pedro amaral <pedro.amaral@fomento.to.gov.br>
2026-05-05 17:50:19 -0300

    add schemas

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,18 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app) #Arrange
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/') #Act
+    response = client.get('/')  # Act
 
-    assert response.status_code == HTTPStatus.OK #Assert
-    assert response.json() == {'message': 'Olá Mundo!'} #Assert
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+
+def test_root_deve_retornar_ola_mundo_html():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/html')  # Act
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text  # Assert
