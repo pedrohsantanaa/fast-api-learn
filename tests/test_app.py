05b1fde0a4960e6ddbb03f3f5ac88f4cacabcc5d
pedro amaral <pedro.amaral@fomento.to.gov.br>
2026-05-14 17:41:24 -0300

    continuos study

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
+def test_root_deve_retornar_ok_e_ola_mundo(client):
 
     response = client.get('/')  # Act
 
@@ -14,10 +9,25 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
 
 
-def test_root_deve_retornar_ola_mundo_html():
-    client = TestClient(app)  # Arrange
-
+def test_root_deve_retornar_ola_mundo_html(client):
     response = client.get('/html')  # Act
 
     assert response.status_code == HTTPStatus.OK
     assert '<h1> Olá Mundo </h1>' in response.text  # Assert
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
